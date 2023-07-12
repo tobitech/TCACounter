d3d9ffce21958cbc2115177e6b46b4f863b6eb29
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 13:00:53 +0100

    Initial Commit

---
+++ TCACounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCACounter
+//
+//  Created by Oluwatobi Omotayo on 12/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCACounter/TCACounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterApp.swift
+//  TCACounter
+//
+//  Created by Oluwatobi Omotayo on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
