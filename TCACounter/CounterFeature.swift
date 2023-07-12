9bf5de386d15bf039bfcabf8edd4fb987f285caa
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 15:02:53 +0100

    integrating with swiftui

--- TCACounter/CounterFeature.swift
+++ TCACounter/CounterFeature.swift
@@ -6,13 +6,14 @@
 //
 
 import ComposableArchitecture
+import SwiftUI
 
 struct CounterFeature: ReducerProtocol {
-	struct State {
+	struct State: Equatable {
 		var count = 0
 	}
 	
-	enum Action {
+	enum Action: Equatable {
 		case decrementButtonTapped
 		case incrementButtonTapped
 	}
@@ -23,10 +24,53 @@ struct CounterFeature: ReducerProtocol {
 		case .decrementButtonTapped:
 			state.count -= 1
 			return .none
-
+			
 		case .incrementButtonTapped:
 			state.count += 1
 			return .none
 		}
+	}
+}
+
+struct CounterView: View {
+	let store: StoreOf<CounterFeature>
+	
+	var body: some View {
+		WithViewStore(self.store, observe: { $0 }) { viewStore in
+			VStack {
+				Text("\(viewStore.count)")
+					.font(.largeTitle)
+					.padding()
+					.background(Color.black.opacity(0.1))
+					.cornerRadius(10)
+				HStack {
+					Button("-") {
+						viewStore.send(.decrementButtonTapped)
+					}
+					.font(.largeTitle)
+					.padding()
+					.background(Color.black.opacity(0.1))
+					.cornerRadius(10)
+					
+					Button("+") {
+						viewStore.send(.incrementButtonTapped)
+					}
+					.font(.largeTitle)
+					.padding()
+					.background(Color.black.opacity(0.1))
+					.cornerRadius(10)
+				}
+			}
+		}
+	}
+}
+
+struct CounterPreview: PreviewProvider {
+	static var previews: some View {
+		CounterView(
+			store: Store(initialState: CounterFeature.State()) {
+				CounterFeature()
+			}
+		)
 	}
 }
