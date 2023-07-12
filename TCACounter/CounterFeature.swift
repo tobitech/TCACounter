82ceb8ca54a4149986f6a7d5051412c4d091d89e
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 14:55:53 +0100

    create a reducer

---
+++ TCACounter/CounterFeature.swift
@@ -0,0 +1,32 @@
+//
+//  CounterFeature.swift
+//  TCACounter
+//
+//  Created by Oluwatobi Omotayo on 12/07/2023.
+//
+
+import ComposableArchitecture
+
+struct CounterFeature: ReducerProtocol {
+	struct State {
+		var count = 0
+	}
+	
+	enum Action {
+		case decrementButtonTapped
+		case incrementButtonTapped
+	}
+	
+	
+	func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+		switch action {
+		case .decrementButtonTapped:
+			state.count -= 1
+			return .none
+
+		case .incrementButtonTapped:
+			state.count += 1
+			return .none
+		}
+	}
+}
