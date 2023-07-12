ceb658bc31c0133a279c7b98947b666cce1d09b2
Tobi Omotayo <tobitech@ymail.com>
2023-07-12 15:12:39 +0100

    integrating into the app

--- TCACounter/TCACounterApp.swift
+++ TCACounter/TCACounterApp.swift
@@ -5,13 +5,21 @@
 //  Created by Oluwatobi Omotayo on 12/07/2023.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCACounterApp: App {
+	static let store = Store(initialState: CounterFeature.State()) {
+		CounterFeature()
+			._printChanges()
+	}
+
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+			CounterView(
+				store: TCACounterApp.store
+			)
 		}
 	}
 }
