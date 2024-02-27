4a676e44e286977215b63a19b17a89840569ea13
Tobi Omotayo <tobitech@ymail.com>
2024-02-27 15:03:15 +0100

    add some side effect to fetch facts from numbers api

--- TCACounter/CounterFeature.swift
+++ TCACounter/CounterFeature.swift
@@ -13,10 +13,14 @@ struct CounterFeature: Reducer {
 	@ObservableState
 	struct State: Equatable {
 		var count = 0
+		var fact: String?
+		var isLoading = false
 	}
 	
 	enum Action: Equatable {
 		case decrementButtonTapped
+		case factButtonTapped
+		case factResponse(String)
 		case incrementButtonTapped
 	}
 	
@@ -25,10 +29,27 @@ struct CounterFeature: Reducer {
 			switch action {
 			case .decrementButtonTapped:
 				state.count -= 1
+				state.fact = nil
+				return .none
+				
+			case .factButtonTapped:
+				state.fact = nil
+				state.isLoading = true
+				return .run { [count = state.count] send in
+					let (data, _) = try await URLSession.shared
+						.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+					let fact = String(decoding: data, as: UTF8.self)
+					await send(.factResponse(fact))
+				}
+				
+			case let .factResponse(fact):
+				state.fact = fact
+				state.isLoading = false
 				return .none
 				
 			case .incrementButtonTapped:
 				state.count += 1
+				state.fact = nil
 				return .none
 			}
 		}
@@ -63,6 +84,22 @@ struct CounterView: View {
 					.background(Color.black.opacity(0.1))
 					.cornerRadius(10)
 				}
+				Button("Fact") {
+					store.send(.factButtonTapped)
+				}
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+			}
+			
+			if store.isLoading {
+				ProgressView()
+			} else if let fact = store.fact {
+				Text(fact)
+					.font(.largeTitle)
+					.multilineTextAlignment(.center)
+					.padding()
 			}
 		}
 	}
