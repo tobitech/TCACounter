d20df050230ae5f4d846c9f34503fe443cb08ad6
Tobi Omotayo <tobitech@ymail.com>
2024-02-27 15:10:42 +0100

    introduce a cancellable timer effect that auto increments the count after every second

--- TCACounter/CounterFeature.swift
+++ TCACounter/CounterFeature.swift
@@ -15,6 +15,7 @@ struct CounterFeature: Reducer {
 		var count = 0
 		var fact: String?
 		var isLoading = false
+		var isTimerRunning = false
 	}
 	
 	enum Action: Equatable {
@@ -22,7 +23,11 @@ struct CounterFeature: Reducer {
 		case factButtonTapped
 		case factResponse(String)
 		case incrementButtonTapped
+		case timerTick
+		case toggleTimerButtonTapped
 	}
+	
+	enum CancelID { case timer }
 	
 	var body: some ReducerOf<Self> {
 		Reduce { state, action in
@@ -51,6 +56,25 @@ struct CounterFeature: Reducer {
 				state.count += 1
 				state.fact = nil
 				return .none
+				
+			case .timerTick:
+				state.count += 1
+				state.fact = nil
+				return .none
+				
+			case .toggleTimerButtonTapped:
+				state.isTimerRunning.toggle()
+				if state.isTimerRunning {
+					return .run { send in
+						while true {
+							try await Task.sleep(for: .seconds(1))
+							await send(.timerTick)
+						}
+					}
+					.cancellable(id: CancelID.timer)
+				} else {
+					return .cancel(id: CancelID.timer)
+				}
 			}
 		}
 	}
@@ -84,6 +108,15 @@ struct CounterView: View {
 					.background(Color.black.opacity(0.1))
 					.cornerRadius(10)
 				}
+				
+				Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+					store.send(.toggleTimerButtonTapped)
+				}
+				.font(.largeTitle)
+				.padding()
+				.background(Color.black.opacity(0.1))
+				.cornerRadius(10)
+				
 				Button("Fact") {
 					store.send(.factButtonTapped)
 				}
